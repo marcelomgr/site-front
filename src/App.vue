dcbb450f2a087b871f772a756b2e6131095c18bb
marcelomgr <rodrigues.marcelog@gmail.com>
2023-01-20 00:25:57 -0300

    Adicionando arquivos

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <HomeComponent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import './styles/global.css'
+import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+import HomeComponent from './pages/HomeComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
+  }
+}
+
+</script>
---
+++ src/pages/HomeComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    
+    <main>
+        <div class="text-top">Videos novos toda quinta as 20 horas</div>
+
+        <a href="http://www.google.com" target="_blank">
+            <img alt="Inscreva-se" id="subscribe" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg">
+        </a>
+
+        <div class="text-bottom">youtube.com/...</div>
+    </main>
+</template>
+
+<script>
+export default{
+    name: 'HomeComponent'
+}
+
+
+</script>
+
+<style scoped>
+
+main{
+    background-color: var(--color-background-home);
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.text-top{
+    color: var(--color-text-light);
+    font-size:22px;
+    margin-bottom: 20px;
+}
+
+#subscribe{
+    width: 340px;
+}
+.text-bottom{
+    color: var(--color-text-light);
+    font-size: 18px;
+    margin-top: 20px;
+}
+
+@media (min-width: 700px){
+    #subscribe{
+        width: 370px;
+    }
+}
+
+</style>
